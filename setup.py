f8af6d8a74b484117a189eeed2b4399ce71b637d
mvinyard <mvinyard@broadinstitute.org>
2022-01-04 16:35:05 +0000

    adding setup files

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import re
+import os
+import sys
+
+
+setup(
+    name="anngtf",
+    version="0.0.0",
+    python_requires=">3.6.0",
+    author="Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard",
+    author_email="mvinyard@broadinstitute.org",
+    url=None,
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    description="anngtf - lift annotations from a `.gtf` file to your AnnData object.",
+    packages=[
+        "anngtf",
+    ],
+    
+    install_requires=[
+	"anndata>=0.7.8",
+	"gtfparse>=1.2.1",
+	"pandas>=1.3.5",
+    ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3.6",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    license="MIT",
+)
