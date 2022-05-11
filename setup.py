e9a9ec7e9593d80159e329538873837a43123220
mvinyard <mvinyard@broadinstitute.org>
2022-05-11 14:59:47 +0000

    v0.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 
 setup(
     name="GTFast",
-    version="0.0.2",
+    version="0.0.3",
     python_requires=">3.6.0",
     author="Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard",
     author_email="mvinyard@broadinstitute.org",
