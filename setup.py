a997f0d286e7853c6513c714a122878c6205917f
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2022-05-10 19:16:24 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ import sys
 
 setup(
     name="GTFast",
-    version="0.0.1",
+    version="0.0.2",
     python_requires=">3.6.0",
     author="Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard",
     author_email="mvinyard@broadinstitute.org",
-    url=None,
+    url="https://github.com/mvinyard/GTFast",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     description="GTFast - cache and subset a `.gtf` file as a `.csv` for faster subsequent use.",
